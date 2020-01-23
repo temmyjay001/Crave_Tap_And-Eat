336b080530ca0b50769557086337fe87943e9701
solomon <temmyjay001@gmail.com>
2020-01-23 14:33:27 +0100

    #4 creation of fragments,bottom navigation and additional features

--- app/src/main/java/com/niit/project/Login.java
+++ app/src/main/java/com/niit/project/Login.java
@@ -13,6 +13,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
@@ -28,23 +33,28 @@ import com.google.firebase.database.ValueEventListener;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Arrays;
+
 public class Login extends AppCompatActivity implements View.OnClickListener{
 
     private static final String TAG = "Crave Tap Eat";
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private CallbackManager callbackManager;
     private ImageButton btRegister;
     private Button btLogin;
     private ImageView circle1;
+    private static final String EMAIL = "email";
     TextView tvLogin;
     EditText ETusername,ETpassword;
     String  email, password;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity);
-
+        callbackManager = CallbackManager.Factory.create();
         mAuth = FirebaseAuth.getInstance();
 
         btRegister = findViewById(R.id.btRegister);
@@ -62,6 +72,32 @@ public class Login extends AppCompatActivity implements View.OnClickListener{
                 email = ETusername.getText().toString();
                 password = ETpassword.getText().toString();
                 signIn(email, password);
+            }
+        });
+
+
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList(EMAIL));
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.d(TAG, "I have logged in for facebook stuff");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+                Log.d(TAG, "I couldn't log in for facebook stuff");
+
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+                Log.d(TAG, exception.toString());
             }
         });
     }
